e4a2eeca20b2d7b0152c14615dcb91b6523363d2
ludmilabenitez <ludmilahabba@gmail.com>
2026-08-06 19:27:53 -0300

    Add: letter density

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import { Header } from "./components/Header.jsx"
 
 function App() {
     const [text, setText] = useState("Design is the silent ambassador of your brand. Simplicity is key to effective communication, creating clarity in every interaction. A great design **transforms** complex ideas into elegant solutions, making them easy to understand. It blends **aesthetics** and functionality seamlessly.")
+
     const [excludeSpaces, setExcludeSpaces] = useState(false)
     const [limitCharacter, setLimitCharacter] = userState(false)
     const [limitValue, setLimitValue] = useState(300)
 
-    
     const characters = excludeSpaces ? text.replace(/\s/g, "").length : "text.length"
-
     const words = text.trim() === "" ? 0 : text.trim().split(/\s+/g).length
-
     const sentences = text.trim() === "" ? 0 : text.split("/[.!?]/").filter(sentence => sentence.trim() !== "").length
+    const readingTime = Math.ceil(words / 200)
 
     const handleChangeTextArea = (e) =>{
         const value = e.target.value
@@ -24,12 +23,28 @@ function App() {
             setText(value)
         }
     }
-
     const handleChangeInputLimit = (e) => {
         setLimitCharacter(!limitCharacter)
         const newText = text.slice(0, limitValue)
         setText(newText)
     }
+
+    const cleanText = text.toLocaleLowerCase().replace(/[^a-záéíóúÜ]/g, "")
+    const total= cleanText.length
+
+
+    const dictionaryLetters = { test: 1 }
+    cleanText.split("").forEach (letter => dictionaryLetters[letter] = (dictionaryLetters[letter] || 0) + 1)
+    const letters = Object.entries(dictionaryLetters).map(dataLetter => {
+        const letter = dataLetter[0]
+        const amountLetter = dataLetter[1]
+        const infoToRenderLetter = {
+            letter: letter,
+            amount: amountLetter,
+            porcentaje: (total/amountLetter)*100,
+        }
+    })
+    const sortLetters = letters.sort((a,b) => b.amount - a.amount)
 
     return (
         <main>
@@ -63,6 +78,21 @@ function App() {
             <p>Cantidad de caracteres: {characters}</p>
             <p>Cantidad de palabras: {words}</p>
             <p>Cantidad de oraciones: {sentences}</p>
+            <p>Tiempo aprox de lectura: {readingTime} minuto/s</p>
+            <section>
+                <h2>Cantidad de letras</h2>
+                <article>
+                    {
+                        letters.map(obj => 
+                            <div key= {obj.letter}>
+                                <span>{obj.letter.toUpperCase()}</span>
+                                <meter min="0" max="100" value={obj.porcentaje}></meter>
+                                <span>{obj.amount}({obj.porcentaje.toFixed(1)}%)</span>
+                            </div>
+                        )
+                    }
+                </article>
+            </section>
         </main>
     )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return <header>
+        <div>
+            <p>C</p>
+            <h1>Character Counter UTN</h1>
+        </div>
+        <button>☀</button>
+    </header>
+}
+
+export { Header }
