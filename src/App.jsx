41480b726452320f0bbda1d6d016e62c578565ef
ludmilabenitez <ludmilahabba@gmail.com>
2026-08-06 16:39:09 -0300

    add: header, textarea and checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react"
+import { Header } from "./components/Header.jsx"
+
 function App() {
+    const [text, setText] = useState("Design is the silent ambassador of your brand. Simplicity is key to effective communication, creating clarity in every interaction. A great design **transforms** complex ideas into elegant solutions, making them easy to understand. It blends **aesthetics** and functionality seamlessly.")
+    const [excludeSpaces, setExcludeSpaces] = useState(false)
+    console.log (excludeSpaces, )
+    return (
+        <main>
+            <Header />
+            <h2>Analyze your text <br /> in real-time.</h2>
+            <textarea 
+                placeholder="Write your text..."
+                onChange={(e) => setText(e.target.value)}
+            value={text}
+            ></textarea>
+            <div>
+                <label>
+                    <input type="checkbox"
+                    checked={excludeSpaces}
+                    onChange={() => setExcludeSpaces(!excludeSpaces)}
+                    />
+                    Excluir espacios
+                </label>
+            </div>
+            <p>Cantidad de caracteres: {text.length}</p>
+        </main>
+    )
 
 }
 
